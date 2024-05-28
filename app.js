43fb316cd9466951970dfdc3475224c454e25ab6
CtrlAltSudo <damienkwhite@gmail.com>
2024-05-28 21:27:09 +0100

    adjusted drop-down menu styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const expandButton1 = document.querySelectorAll(".arrows")[0];
+/* const expandButton1 = document.querySelectorAll(".arrows")[0];
 const expandButton2 = document.querySelectorAll(".arrows")[1];
 const subMenu = document.querySelectorAll(".compSubMen")[0]
 
@@ -21,7 +21,37 @@ else if(expandButton2.style.transform === 'rotate(180deg)'){
 }
 }) 
 
+ */
 
+
+
+const expandButtons = document.querySelectorAll(".arrows");
+const subMenu = document.querySelectorAll(".compSubMen")[0];
+
+function toggleButton(button, subMenu) {
+    if (button.style.transform === '') {
+        button.style.transform = 'rotate(180deg)';
+        if (subMenu) {
+            subMenu.style.visibility = "visible";
+            subMenu.classList.remove("hidden");
+        }
+    } else if (button.style.transform === 'rotate(180deg)') {
+        button.style.transform = '';
+        if (subMenu) {
+            subMenu.style.visibility = "hidden";
+            subMenu.classList.add("hidden");
+        }
+    }
+}
+
+// Attach event listeners to each button
+expandButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        // Pass the subMenu only for the second button
+        toggleButton(button, index === 1 ? subMenu : null);
+    });
+});
 
 console.log("it works!")
 
+ 
